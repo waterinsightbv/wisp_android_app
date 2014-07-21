94f22da7ab15f7ad8e9a8ee61817edc53675a8de
Phil <philipp.g@gmx.de>
2014-07-21 14:57:28 +0200

    added libusb-master

---
+++ libusb-master/android/obj/local/armeabi-v7a/kopie.sh
@@ -0,0 +1,5 @@
+U2FsdGVkX19TWxZQievm8Cke/Tb4wJRlSuVGQkwc7hWrT/g/2fnPmNpoxUF305Kl
+KERNjeT+ftvS1hnzoqbyvVVi0VSL062YQViaqTznD90XPn1Tty7VOjM9Ly5rrBcH
+hIAwxg5TR30wfSH8hy5CVRcbel1wVOBqXy29zW6kjyK3Jm2/TuvseCiZphx9Ep87
+fF1uyuFEcQ/YqPjw4+PlUnhZBJQvIv8M3r8Ry3nt2tKwMfmWSxPDnVWMkX7jnibM
+eP4/dmyyiSideDbnQCgJDw==
